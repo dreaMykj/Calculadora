9eba391dc0ab3b44e771f37a9e13b82bdebc06a3
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-20 22:28:59 -0300

    Alteracao concluida

--- Program.cs
+++ Program.cs
@@ -1,77 +1,48 @@
-﻿double numero1, numero2, resultado = 0;
+﻿double numero1 = 0;
+double numero2 = 0;
+double resultado = 0;
 string operacao;
 string sair;
 
-ExibeMenu();
-
-Console.WriteLine();
-Console.WriteLine("Aguarde...");
-Console.WriteLine();
-
-numero1 = SolicitaNumero("primeiro");
-
-Console.WriteLine("Escolha uma operação: ");
-Console.ForegroundColor = ConsoleColor.Green;
-Console.ResetColor();
-operacao = Console.ReadLine();
-
-numero2 = SolicitaNumero("segundo");
-Console.WriteLine();
-
 do
 {
-switch(operacao)
-    {
-    case "+":
-    resultado = numero1 + numero2;
-    Console.Write("O valor da soma é: ");
-    break;
-    case "-":
-    resultado = numero1 - numero2;
-    Console.Write("O valor da subtração é: ");
-    break;
-    case "/":
-    resultado = numero1 / numero2;
-    Console.Write("O valor da divisão é: ");
-    break;
-    case "*":
-    resultado = numero1 * numero2;
-    Console.Write("O valor da multiplicação é: ");
-    break;
-    case "^":
-    resultado = Math.Pow(numero1, numero2);
-    Console.Write("O valor da exponeciação é: ");
-    break;
-    case "R":
-    numero2 = Convert.ToDouble(numero1);
-    resultado = Math.Sqrt(numero2);
-    Console.WriteLine($"A raiz quadrada de {numero2} é: ");
-    break;
-    case "r":
-    numero2 = Convert.ToDouble(numero1);
-    resultado = Math.Sqrt(numero2);
-    Console.WriteLine($"A raiz quadrada de {numero2} é: ");
-    break;
+ExibeMenu();
 
-    default:
-    Console.WriteLine("Operação incorreta!");
-    break;
-    }
+ExibeOpcoes();
+    resultado = ExecutaOperacao(operacao, numero1, numero2);
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("{0}", resultado);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("{0}", resultado);
 
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("Deseja sair? S/N: ");
-Console.WriteLine();
-Console.ResetColor();
-sair = Console.ReadLine().ToUpper();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Deseja sair? S/N: ");
+    Console.WriteLine();
+    Console.ResetColor();
+    sair = Console.ReadLine().ToUpper();
 }
 while (sair != "S" && sair != "SIM");
 {
     Console.WriteLine();
     Console.WriteLine("\nVolte sempre!");
 }
+
+void ExibeOpcoes()
+{
+    Console.WriteLine();
+    Console.WriteLine("Aguarde...");
+    Console.WriteLine();
+
+    numero1 = SolicitaNumero("primeiro");
+
+    Console.WriteLine("Escolha uma operação: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.ResetColor();
+    operacao = Console.ReadLine();
+
+    numero2 = SolicitaNumero("segundo");
+    Console.WriteLine();
+}
+
 void ExibeMenu()
 {
     Console.ForegroundColor = ConsoleColor.Green;
@@ -97,7 +68,6 @@ void ExibeMenu()
     Console.ResetColor();
 }
 
-
 double SolicitaNumero(string ordemNumero)
 {
     double numero = 0;
@@ -108,15 +78,82 @@ double SolicitaNumero(string ordemNumero)
     Console.Write(ordemNumero);
     Console.ResetColor();
 
-    Console.WriteLine("número");
+    Console.WriteLine(" número");
 
     numero = Convert.ToDouble(Console.ReadLine());
 
     return numero;
 }
 
-double Soma(double numero01, double numero02);
+double Soma(double numero01, double numero02)
 {
-    
-   return numero01 + numero02;
+    return numero01 + numero02;
+}
+
+double Subtracao(double numero01, double numero02)
+{
+    return numero01 - numero02;
+}
+
+double Divisao(double dividendo, double divisor)
+{
+    return dividendo / divisor;
+}
+
+double Multiplicacao(double numero01, double numero02)
+{
+    return numero01 * numero02;
+}
+
+double Exponenciacao(double numero01, double numero02)
+{
+    double Exponenciacao = Math.Pow(numero01, numero02);
+    return Exponenciacao;
+}
+
+double RaizQuadrada(double numero01, double numero02)
+{
+    double RaizQuadrada = Math.Sqrt(numero01);
+    return RaizQuadrada;
+}
+
+double ExecutaOperacao(string operacao, double numero01, double numero02)
+{
+    double resultado = 0;
+    switch (operacao)
+    {
+        case "+":
+            resultado = Soma(numero01, numero02);
+            Console.Write("O valor da soma é: ");
+            break;
+        case "-":
+            resultado = Subtracao(numero01, numero02);
+            Console.Write("O valor da subtração é: ");
+            break;
+        case "/":
+            resultado = Divisao(numero01, numero02);
+            Console.Write("O valor da divisão é: ");
+            break;
+        case "*":
+            resultado = Multiplicacao(numero01,numero02);
+            Console.Write("O valor da multiplicação é: ");
+            break;
+        case "^":
+            resultado = Exponenciacao(numero01,numero02);
+            Console.Write("O valor da exponeciação é: ");
+            break;
+        case "R":
+            resultado = RaizQuadrada(numero01,numero02);
+            Console.WriteLine($"A raiz quadrada de {numero01} é: ");
+            break;
+        case "r":
+            resultado = RaizQuadrada(numero01,numero02);
+            Console.WriteLine($"A raiz quadrada de {numero01} é: ");
+            break;
+
+        default:
+            Console.WriteLine("Operação incorreta!");
+            break;
+    }
+    return resultado;
 }
